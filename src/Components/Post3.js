9559a13fba6c9f8745554e3cd1576c6a765db40b
Powsteena <marypowsteenaj@gmail.com>
2024-07-24 11:21:07 +0530

    third commit

--- src/Components/Post3.js
+++ src/Components/Post3.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import img3 from './algorithm.png'
 
 const Post1 = () => {
   return (
     <div className="card" style={{ margin: '40px 150px ', width: '60%' }}>
-      <img src="l" alt="Post 3 Image" />
+      <img src={img3} alt="Post 3 Image" />
       <h2>Alogorithm</h2>
       <p>jjjjjjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjjjjjjjjjjjjj</p>
       <button>Read more</button>
--- src/Components/Post4.js
+++ src/Components/Post4.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import img4 from './computer network.jpeg'
 
 const post4 = () => {
   return (
     <div className="card" style={{ margin: '40px 150px ', width: '60%' }}>
-      <img src="o" alt="Post 4 Image" />
+      <img src={img4} alt="Post 4 Image" />
       <h2>Computer Network</h2>
       <p>eeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee </p>
       <button>Read more</button>
